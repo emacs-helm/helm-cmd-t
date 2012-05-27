f0a9495569c4f56de7faa43183477866510ff60a
Le Wang <le.wang@agworld.com.au>
2012-05-27 21:22:08 +0800

    rename to cmd-t, commentary update

--- helm-cmd-t.el
+++ helm-cmd-t.el
@@ -1,21 +1,21 @@
-;;; helm-project-files.el --- cmd-t style completion of files in project
+;;; helm-cmd-t.el --- cmd-t style completion
 
 ;; this file is not part of Emacs
 
-;; Copyright (C) 2011 Le Wang
+;; Copyright (C) 2011, 2012 Le Wang
 ;; Author: Le Wang
 ;; Maintainer: Le Wang
-;; Description: cmd-t style completion of files in project
+;; Description: cmd-t style completion of files in repository
 ;; Author: Le Wang
 ;; Maintainer: Le Wang
 
 ;; Created: Sat Nov  5 16:42:32 2011 (+0800)
 ;; Version: 0.1
-;; Last-Updated: Sun May 27 20:31:43 2012 (+0800)
+;; Last-Updated: Sun May 27 21:09:38 2012 (+0800)
 ;;           By: Le Wang
-;;     Update #: 108
-;; URL: https://github.com/lewang/helm-project-files
-;; Keywords: helm project file-list completion convenience cmd-t textmate slickedit
+;;     Update #: 111
+;; URL: https://github.com/lewang/helm-cmd-t
+;; Keywords: helm project-management completion convenience cmd-t textmate
 ;; Compatibility:
 
 ;;; Installation:
@@ -25,8 +25,8 @@
 ;; 2. add to your config
 ;;
 ;;      (require 'helm-config)
-;;      (require 'helm-project-files)
-;;      (define-key (current-global-map) [remap switch-to-buffer] 'helm::project-files)
+;;      (require 'helm-cmd-t)
+;;      (define-key (current-global-map) [remap switch-to-buffer] 'helm-cmd-t)
 ;;
 ;; 3. install find_interesting script to an executable path of your choosing:
 ;;    e.g. /usr/local/bin
@@ -45,15 +45,15 @@
 ;;; Commentary:
 
 ;; This is yet another cmd-t package.  Fast file-name completion from the
-;; current "project".  The concept of a "project" is configurable through
-;; `helm::pf-root-types'.
+;; current "repository".  The concept of a "respository" is configurable through
+;; `helm-cmd-t-repo-types'.
 ;;
 ;; It's highly recommended that you add an helm source like recentf that keeps
 ;; track of recent files you're created.  This way, you don't have to worry
-;; about your project cache being out of date, the files you edit using Emacs
+;; about your respository cache being out of date, the files you edit using Emacs
 ;; appear through the recentf source.
 ;;
-;; In fact, `helm::pf-find' should be used as a drop-in replacement for
+;; In fact, `helm-cmd-t' should be used as a drop-in replacement for
 ;; `switch-to-buffer' or "C-x b".
 ;;
 ;; A word on ido style "flex" matching: meh.  I haven't found it very useful
@@ -88,87 +88,87 @@
 (eval-when-compile (require 'cl))
 
 
-(provide 'helm-project-files)
+(provide 'helm-cmd-t)
 (require 'helm-config)
 
-(defvar helm::pf-data nil
-  "project files data only relevant in helm source buffer.")
-(make-variable-buffer-local 'helm::pf-data)
+(defvar helm-cmd-t-data nil
+  "data only relevant in helm source buffer.")
+(make-variable-buffer-local 'helm-cmd-t-data)
 
-(defvar helm::pf-default nil
+(defvar helm-cmd-t-default-repo nil
   "A path that points to a default project root.
 If the current file does not belong to a project then this path is used.
 ")
 
-(defvar helm::pf-command "find_interesting"
+(defvar helm-cmd-t-command "find_interesting"
   "command to execute to get list of files it should be some variant of the Unix `find' command.")
 
-(defvar helm::pf-root-types
+(defvar helm-cmd-t-repo-types
   `((git . "git --no-pager ls-files --full-name -- %s")
-    (hg . ,(concat helm::pf-command " %s"))
-    (bzr . ,(concat helm::pf-command " %s"))
-    (dir-locals.el . ,(concat helm::pf-command " %s")))
+    (hg . ,(concat helm-cmd-t-command " %s"))
+    (bzr . ,(concat helm-cmd-t-command " %s"))
+    (dir-locals.el . ,(concat helm-cmd-t-command " %s")))
   "root types supported.
 this is an alist of (type . \"format-string\") the project root is used to format the command ")
 
-(defvar helm::pf-hints (mapcar (lambda (root-type)
-                                 (concat "." (symbol-name (car root-type))))
-                               helm::pf-root-types)
-  "A list of files considered to mark the root of a project")
+(defvar helm-cmd-t-cookies (mapcar (lambda (repo-type)
+                                 (concat "." (symbol-name (car repo-type))))
+                               helm-cmd-t-repo-types)
+  "A list of files that mark the root of a repository")
 
-(defvar helm::pf-sources '(helm-c-source-buffers-list
-                           helm-c-source-recentf
-                           helm-c-source-files-in-current-dir
-                           helm::pf-source
-                           helm-c-source-buffer-not-found)
-  "list of sources for `helm::pf-find'
+(defvar helm-cmd-t-sources '(helm-c-source-buffers-list
+                             helm-c-source-recentf
+                             helm-c-source-files-in-current-dir
+                             helm-cmd-t-source
+                             helm-c-source-buffer-not-found)
+  "list of sources for `helm-cmd-t-find'
 
-helm::pf-source is a place-holder.
+helm-cmd-t-source is a place-holder.
 ")
 
-(defvar helm::pf-anti-hint ".emacs-helm-no-spider"
+(defvar helm-cmd-t-anti-hint ".emacs-helm-no-spider"
   "Marker file that disqualifies a directory from being considered a project.")
 
-(defvar helm::pf-source-buffer-format
-  " *helm::pf source - [%s]*")
+(defvar helm-cmd-t-source-buffer-format
+  " *helm-cmd-t source - [%s]*")
 
-(defun helm::pf-root (&optional buff)
+(defun helm-cmd-t-root (&optional buff)
   "return project root of buffer as string"
   (with-current-buffer (or buff
                            helm-current-buffer
                            (current-buffer))
-    (cdr (helm::pf-root-data))))
+    (cdr (helm-cmd-t-root-data))))
 
-(defun helm::pf-root-data (&optional file)
+(defun helm-cmd-t-root-data (&optional file)
   "get project directory of file
 return (<repo type> . <root.)"
   (setq file (or file
                  default-directory))
   (let (res)
-    (loop for hint-file in helm::pf-hints
+    (loop for hint-file in helm-cmd-t-cookies
           do (when (setq res (locate-dominating-file file hint-file))
-               (when (file-exists-p (expand-file-name helm::pf-anti-hint res))
-                 (setq res helm::pf-default))
+               (when (file-exists-p (expand-file-name helm-cmd-t-anti-hint res))
+                 (setq res helm-cmd-t-default-repo))
                (return (cons (intern (replace-regexp-in-string "\\`\\.+" "" hint-file))
                              (directory-file-name res)))))))
 
-(defun helm::pf-sources ()
+(defun helm-cmd-t-sources ()
   "return a list of sources appropriate for use with helm.
 
-helm::pf-source is replaced with an appropriate item .
+helm-cmd-t-source is replaced with an appropriate item .
 "
-  (let* ((my-sources (append helm::pf-sources '()))
-         (my-root-data (funcall 'helm::pf-root-data))
+  (let* ((my-sources (append helm-cmd-t-sources '()))
+         (my-root-data (funcall 'helm-cmd-t-root-data))
          (my-root (cdr my-root-data))
-         (my-source-buffer-name (helm::pf-get-source-buffer-name my-root))
+         (my-source-buffer-name (helm-cmd-t-get-source-buffer-name my-root))
          (candidates-buffer (get-buffer-create my-source-buffer-name))
          (my-source (with-current-buffer candidates-buffer
-                      (cdr (assq 'helm-source helm::pf-data)))))
+                      (cdr (assq 'helm-source helm-cmd-t-data)))))
     (unless my-source
       (with-current-buffer candidates-buffer
         (erase-buffer)
         ;; hard code the git for testing
-        (shell-command (format (helm::pf-get-listing-command my-root-data) my-root) t)
+        (shell-command (format (helm-cmd-t-get-listing-command my-root-data) my-root) t)
         (setq my-source `((name . ,(format "project files [%s]" my-root))
                           (init . ,(lexical-let ((candidates-buffer candidates-buffer))
                                      #'(lambda ()
@@ -176,33 +176,33 @@ helm::pf-source is replaced with an appropriate item .
                           (candidates-in-buffer)
                           (match helm-c-match-on-file-name
                                  helm-c-match-on-directory-name)
-                          (action . helm::pf-find-file)
+                          (action . helm-cmd-t-find-file)
                           (type . file)))
-        (setq helm::pf-data (list (cons 'helm-source my-source)
+        (setq helm-cmd-t-data (list (cons 'helm-source my-source)
                                   (cons 'project-root my-root)))))
-    (setcar (memq 'helm::pf-source my-sources) my-source)
+    (setcar (memq 'helm-cmd-t-source my-sources) my-source)
     my-sources))
 
 
-(defun helm::pf-find-file (candidate)
-  (interactive)
+(defun helm-cmd-t-find-file (candidate)
+  "find file"
   (find-file (expand-file-name candidate
                                (with-current-buffer (helm-candidate-buffer)
-                                 (cdr (assq 'project-root helm::pf-data))))))
+                                 (cdr (assq 'project-root helm-cmd-t-data))))))
 
-(defun helm::pf-get-source-buffer-name (root)
-  (format helm::pf-source-buffer-format root))
+(defun helm-cmd-t-get-source-buffer-name (root)
+  (format helm-cmd-t-source-buffer-format root))
 
-(defun helm::pf-get-listing-command (root-data)
-  (cdr (assoc (car root-data) helm::pf-root-types)))
+(defun helm-cmd-t-get-listing-command (root-data)
+  (cdr (assoc (car root-data) helm-cmd-t-repo-types)))
 
-(defun helm::project-files (arg)
+(defun helm-cmd-t (arg)
   "This command is designed to be a drop-in replacement for switch to buffer.
 
 With universal prefix arg C-u, invalidate cache for current project first.
 
 You can configure which sources are used through the
-`helm::pf-sources' variable.
+`helm-cmd-t-sources' variable.
 
 It is important to add a source that keeps track of files you
 work with (e.g. `recentf').  This way, you don't have to worry about keeping the
@@ -210,23 +210,23 @@ cached list of project files up-to-date.
 "
   (interactive "P")
   (when (consp arg)
-    (helm::pf-invalidate-cache (helm::pf-root)))
+    (helm-cmd-t-invalidate-cache (helm-cmd-t-root)))
   (let ((helm-ff-transformer-show-only-basename nil))
-    (helm :sources (helm::pf-sources)
+    (helm :sources (helm-cmd-t-sources)
           :candidate-number-limit 10
           :buffer "*helm-project-find:*")))
 
-(defun helm::pf-invalidate-cache (root)
+(defun helm-cmd-t-invalidate-cache (root)
   "Invalidate the cached file-list for ROOT."
   (interactive (let ((regexp (replace-regexp-in-string
                               "%s" "\\\\(.*\\\\)"
-                              (regexp-quote helm::pf-source-buffer-format)))
-                     (curr-root (helm::pf-root))
+                              (regexp-quote helm-cmd-t-source-buffer-format)))
+                     (curr-root (helm-cmd-t-root))
                      roots)
                  (mapc (lambda (buf)
                          (with-current-buffer buf
                            (let ((b-name (buffer-name)))
-                             (when (and helm::pf-data
+                             (when (and helm-cmd-t-data
                                         (string-match regexp b-name))
                                (push (match-string-no-properties 1 b-name) roots)))))
                        (buffer-list))
@@ -235,7 +235,7 @@ cached list of project files up-to-date.
                                        :must-match t
                                        :preselect (and (member curr-root roots)
                                                        curr-root)))))
-  (let ((buffer (helm::pf-get-source-buffer-name root)))
+  (let ((buffer (helm-cmd-t-get-source-buffer-name root)))
     (and (buffer-live-p buffer)
          (kill-buffer buffer))))
 
